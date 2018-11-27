cb63f65405a2ed87ced2b8ff24dd98791fba1c90
kamal1203 <ka8846r@greenwich.ac.uk>
2018-11-27 17:11:36 +0000

    So this is the updated version, PUT ALL YOUR OBJECTS IN THE SCENE OF    Assets\SteamVR\InteractionSystem\Samples/INTERACTION EXAMPLE SCENE there is our game

---
+++ Assets/Scripts/RandomStuff/BeltFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeltFollow : MonoBehaviour {
+
+    public Transform Player;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = Player.position;
+	}
+	    
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3 (Player.position.x,Player.position.y - 0.30f, Player.position.z);
+
+    }
+}
