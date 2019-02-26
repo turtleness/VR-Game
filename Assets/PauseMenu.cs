3802ec1e8a60e6a3389fd06ede61df9736e9987e
lordmikez <michaelsimon@live.co.uk>
2019-02-26 15:01:51 +0000

    Adding Early UI

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    // Use this for initialization
+    public static bool GameStopped = false;
+
+    public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameStopped)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+            }
+        }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameStopped = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameStopped = true;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game Has Quit!");
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+
+    }
+}
+
+//add to audieManager
+//if (PauseMenu.GameStopped)
+//    {
+//    s.source.pitch *=.5f;
+//    }
